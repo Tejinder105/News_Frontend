12ebe4136de43eac3bc117b6d6757d3f4dd8bb7a
Tejinder <tejinderp105@gmail.com>
2025-06-26 21:10:26 +0530

    Add CardAd component for  sponsored content

---
+++ Singh_Channel/src/Components/Ui/CardAd.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+function CardAd({ data }) {
+  return (
+    <div className="rounded-xl  overflow-hidden bg-white shadow-md hover:shadow-lg transition-shadow"> 
+      <div className="w-full">
+        <img
+          src={data.image}
+          alt={data.name}
+          className="h-38 w-full object-cover"
+        />
+      </div>
+      <div className="px-3">
+        <h3 className="text-lg font-bold text-gray-900 truncate">{data.headline}</h3>
+        <p className="mb-2 text-sm text-gray-700 line-clamp-2">{data.description}</p>
+        <div className="mb-2 flex items-center justify-between gap-2">
+          <div>
+            <span className="text-xs font-semibold text-gray-500">
+              {data.name}
+            </span>
+            <span className="text-xs text-gray-400">| Sponsored</span>
+          </div>
+        <a
+          href={data.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block rounded-full border border-black px-4 py-1 text-xs font-semibold text-black transition hover:bg-black hover:text-white"
+        >
+          {data.tag}
+        </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardAd;
