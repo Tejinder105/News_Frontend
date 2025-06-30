5a9dc4d083cb8940ea71c0657420b38370e1fb61
Tejinder <tejinderp105@gmail.com>
2025-06-30 08:21:32 +0530

    Refactor Button component, add new Input component

--- Singh_Channel/src/Components/Ui/Button.jsx
+++ Singh_Channel/src/Components/Ui/Button.jsx
@@ -3,31 +3,17 @@ import React from "react";
 function Button({
   children,
   type = "button",
-  classname = "",
-  size = "medium",
-  variant = "default",
-  iconLeft,
-  iconRight,
+  bgColor = "bg-blue-500",
+  textColor = "text-white",
+  className = "",
   ...props
 }) {
-  const baseClasses =
-    "flex items-center justify-center gap-1.5 rounded-md transition-colors duration-300 focus-outline-none  text-base font-semibold";
-
-    const variantClasses={
-        default:"text-white",
-        outline:"bg-transparent text-white hover:bg-blue-400",
-        ghost:"bg-transparent backdrop-blur-md border border-white/20 text-white hover:white/10 "
-    }
-    const sizeClasses = {
-    small: "px-3 py-1.5 text-sm",
-    medium: "px-4 py-2 text-base",
-    large: "px-6 py-3 text-lg",
-  };
   return (
-    <div type={type} className={`${baseClasses} ${classname} ${variantClasses[variant]} ${sizeClasses[size]} `} {...props}>
-      {iconLeft && <span className="flex-shrink-0">{iconLeft}</span>}
-      {children && <span>{children}</span>}
-      {iconRight && <span className="flex-shrink-0">{iconRight}</span>}
+    <div
+      className={`rounded-lg px-4 py-2 ${className} ${textColor} ${bgColor}`}
+      {...props}
+    >
+      {children}
     </div>
   );
 }
--- Singh_Channel/src/Components/Ui/Spinner.jsx
+++ Singh_Channel/src/Components/Ui/Spinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Spinner() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white">
+    <div className="flex min-h-screen  items-center justify-center bg-white">
       <div className="h-12 w-12 animate-spin rounded-full border-4 border-blue-500 border-t-transparent"></div>
     </div>
   );
