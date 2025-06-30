1a860db219014c09354800bbaa3c67207ecb9d51
Tejinder <tejinderp105@gmail.com>
2025-06-30 19:03:32 +0530

    Implement responsive sidebar in AdminLayout with resize handling

--- Singh_Channel/src/Layouts/AdminLayout.jsx
+++ Singh_Channel/src/Layouts/AdminLayout.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../Components/Admin/Sidebar";
 
 function AdminLayout() {
+  const [sideOpen, setSideOpen] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSideOpen(window.innerWidth >= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div>AdminLayout</div>
-  )
+    <div className="relative min-h-screen w-full  bg-slate-100">
+      <Sidebar open={sideOpen} setOpen={setSideOpen} />
+
+      <main
+        className={`h-full overflow-y-auto transition-all duration-300 ${
+          sideOpen ? "pl-72" : "pl-20"
+        }`}
+      >
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
-export default AdminLayout
+
+export default AdminLayout;
