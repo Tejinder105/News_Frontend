754f2952ed753d32865b5ccf7b9a2f2ea6a51ed2
Tejinder <tejinderp105@gmail.com>
2025-07-18 21:01:46 +0530

    Refactor Spinner component

--- Singh_Channel/src/Components/Ui/Spinner.jsx
+++ Singh_Channel/src/Components/Ui/Spinner.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 function Spinner() {
   return (
-    <div className="flex min-h-screen  items-center justify-center bg-white">
-      <div className="h-12 w-12 animate-spin rounded-full border-4 border-blue-500 border-t-transparent"></div>
+    <div className="flex items-center justify-center">
+      <div className="relative h-5 w-5">
+        <div className="absolute h-full w-full rounded-full border-3 border-gray-200/50"></div>
+        <div className="absolute h-full w-full animate-spin rounded-full border-3 border-transparent border-t-white border-r-white"></div>
+      </div>
     </div>
   );
 }
