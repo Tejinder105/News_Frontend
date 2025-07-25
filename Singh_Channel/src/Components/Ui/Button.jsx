b6c1eb7966f419aa84a00bd48326bdd4cec9b09c
Tejinderp105 <tejinderpalsinghc2@gmail.com>
2025-07-25 10:26:35 +0530

    Enhance Button component to support left and right icons with updated styling

--- Singh_Channel/src/Components/Ui/Button.jsx
+++ Singh_Channel/src/Components/Ui/Button.jsx
@@ -6,15 +6,19 @@ function Button({
   bgColor = "bg-blue-500",
   textColor = "text-white",
   className = "",
+  iconLeft,
+  iconRight,
   ...props
 }) {
   return (
     <button
       type={type} 
-      className={`rounded-lg px-4 py-2 ${className} ${textColor} ${bgColor} `}
+      className={`flex items-center gap-2 rounded-lg px-2 py-1 ${className} ${textColor} ${bgColor} `}
       {...props}
     >
+      {iconLeft && iconLeft}
       {children}
+      {iconRight && iconRight}
     </button>
   );
 }
