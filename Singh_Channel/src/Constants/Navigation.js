2f9a162ebc85275a82ce08671588a4e4fde90136
Tejinderp105 <tejinderpalsinghc2@gmail.com>
2025-07-25 10:30:21 +0530

    Add Navigation constants for main navigation items

---
+++ Singh_Channel/src/Constants/Navigation.js
@@ -0,0 +1,7 @@
+export const navItems = [
+    { name: "Home", path: "/" },
+    { name: "News", path: "/news" },
+    { name: "Events", path: "/events" },
+    { name: "Business", path: "/business" },
+    { name: "Directory", path: "/directory" },
+];
