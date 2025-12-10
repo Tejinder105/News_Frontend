fc5596cd915f98ad8fcce3fc0965757472856662
Tejinderp105 <tejinderpalsinghc2@gmail.com>
2025-12-10 15:32:17 +0530

    fix: api error

--- Singh_Channel/src/Services/Store/apiSlice.js
+++ Singh_Channel/src/Services/Store/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
+        baseUrl: (import.meta.env.VITE_API_URL || "http://localhost:8000") + "/api/v1",
     }),
     tagTypes: ["Article"],
     endpoints: (builder) => ({
