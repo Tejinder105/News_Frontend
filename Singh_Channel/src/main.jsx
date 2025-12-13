5b88c7c04db8566d5102463e4409014cb042cb7c
Tejinderp105 <tejinderpalsinghc2@gmail.com>
2025-12-13 22:39:01 +0530

    feat: Preload critical fonts and clean up font imports in CSS

--- Singh_Channel/src/main.jsx
+++ Singh_Channel/src/main.jsx
@@ -4,7 +4,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./Services/store/store.js";
+import store from "./Services/store/store";
 
 createRoot(document.getElementById("root")).render(
     <Auth0Provider
